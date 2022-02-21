40eb6f41d3ee166d47a75266c2db8a713dd7b54a
ychxexn <81912261+ychxexn@users.noreply.github.com>
2022-02-21 17:28:09 +0900

    어노테이션 메모

--- todo/src/main/java/com/example/todo/model/TodoEntity.java
+++ todo/src/main/java/com/example/todo/model/TodoEntity.java
@@ -5,9 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// 생성자와 비슷하게 Builder 패턴으로 객체 생성 가능
 @Builder
+// 매개변수 없는 생성자
 @NoArgsConstructor
+// 모든 멤버 변수를 매개변수로 받는 생성자
 @AllArgsConstructor
+// getter/setter 구현
 @Data
 public class TodoEntity {
 	private String id;
