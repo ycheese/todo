bf033b12cb265db2bcb7be9c460d238f22c2c1da
ychxexn <81912261+ychxexn@users.noreply.github.com>
2022-02-22 22:39:21 +0900

    REST API 실습

--- todo/src/main/java/com/example/todo/controller/TestController.java
+++ todo/src/main/java/com/example/todo/controller/TestController.java
@@ -1,10 +1,18 @@
 package com.example.todo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.todo.dto.ResponseDTO;
+import com.example.todo.dto.TestRequestBodyDTO;
 
 @RestController
 @RequestMapping("test")	// 리소스
@@ -28,5 +36,27 @@ public class TestController {
 	@GetMapping("/textRequestParam")
 	public String testControllerRequestParam(@RequestParam(required=false)int id) {
 		return "Hello World! ID ==> " + id;
+	}
+	
+	@GetMapping("/testRequestBody")
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+		return "Hello World! Id : " + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
+	}
+	
+	@GetMapping("/testResponseBody")
+	public ResponseDTO<String> testControllerResponseBody(){
+		List<String> list = new ArrayList<>();
+		list.add("Hello World! I'm ResponseDTO");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return response;
+	}
+	
+	@GetMapping("/textResponseEntity")
+	public ResponseEntity<?> testControllerResponseEntity(){
+		List<String> list = new ArrayList<>();
+		list.add("Hello World! I'm ResponseEntity. And you got 400!");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		
+		return ResponseEntity.badRequest().body(response);
 	}
 }
