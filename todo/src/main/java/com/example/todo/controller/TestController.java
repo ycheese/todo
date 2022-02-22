c258718cefe070808440f96ffd3c3255370b6c8b
ychxexn <81912261+ychxexn@users.noreply.github.com>
2022-02-22 22:09:33 +0900

    REST API 실습 - TestController 추가

---
+++ todo/src/main/java/com/example/todo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.todo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")	// 리소스
+public class TestController {
+
+	@GetMapping
+	public String testController() {
+		return "Hello World !";
+	}
+}
