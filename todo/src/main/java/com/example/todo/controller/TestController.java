98ec6a139e110bac64027902fc89df8b7420dccc
ychxexn <81912261+ychxexn@users.noreply.github.com>
2022-02-22 22:24:09 +0900

    REST API 실습 - 매개변수2

--- todo/src/main/java/com/example/todo/controller/TestController.java
+++ todo/src/main/java/com/example/todo/controller/TestController.java
@@ -3,6 +3,7 @@ package com.example.todo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +22,11 @@ public class TestController {
 	
 	@GetMapping("/{id}")
 	public String testControllerWitPathVariables(@PathVariable(required=false)int id) {
+		return "Hello World! ID ==> " + id;
+	}
+	
+	@GetMapping("/textRequestParam")
+	public String testControllerRequestParam(@RequestParam(required=false)int id) {
 		return "Hello World! ID ==> " + id;
 	}
 }
