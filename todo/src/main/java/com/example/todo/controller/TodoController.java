ee40823f6c9da187151f6c1198571c04db1887aa
ychxexn <81912261+ychxexn@users.noreply.github.com>
2022-02-23 23:37:38 +0900

    비즈니스 로직 작성 실습

--- todo/src/main/java/com/example/todo/controller/TodoController.java
+++ todo/src/main/java/com/example/todo/controller/TodoController.java
@@ -3,21 +3,27 @@ package com.example.todo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.todo.dto.ResponseDTO;
+import com.example.todo.service.TodoService;
 
 @RestController
 @RequestMapping("todo")
 public class TodoController {
+	
+	@Autowired
+	private TodoService service;
 
-	@GetMapping
+	@GetMapping("/test")
 	public ResponseEntity<?> testTodo(){
+		String str = service.testService();
 		List<String> list = new ArrayList<>();
-		list.add("준비중입니다...");
+		list.add(str);
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		
 		return ResponseEntity.badRequest().body(response);
