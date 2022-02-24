14b5f5e663a2f2eb756437ae16bddc82aa144acc
ychxexn <81912261+ychxexn@users.noreply.github.com>
2022-02-24 23:24:01 +0900

    JPA 쿼리 작성 실습

--- todo/src/main/java/com/example/todo/persistence/TodoRepository.java
+++ todo/src/main/java/com/example/todo/persistence/TodoRepository.java
@@ -1,6 +1,9 @@
 package com.example.todo.persistence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.todo.model.TodoEntity;
@@ -8,4 +11,7 @@ import com.example.todo.model.TodoEntity;
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {	// 매핑될 엔티티 클래스, 기본키 타입
 
+	@Query("select * from Todo t where t.userId = ?1")
+	List<TodoEntity> findByUserId(String userId);
+	
 }
