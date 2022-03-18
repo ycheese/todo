667706a4aa4dd317eb9ffcd338e29c8e2fd580f2
ychxexn <81912261+ychxexn@users.noreply.github.com>
2022-03-18 10:34:52 +0900

    signin 토큰 생성 및 밚환 추가

--- todo/src/main/java/com/example/todo/controller/UserController.java
+++ todo/src/main/java/com/example/todo/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.todo.dto.ResponseDTO;
 import com.example.todo.dto.UserDTO;
 import com.example.todo.model.UserEntity;
+import com.example.todo.security.TokenProvider;
 import com.example.todo.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,9 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private TokenProvider tokenProvider;
 	
 	@PostMapping("/signup")
 	public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO){
@@ -54,9 +58,12 @@ public class UserController {
 		UserEntity user = userService.getByCredentials(userDTO.getEmail(), userDTO.getPassword());
 		
 		if(user != null) {
+			// 토큰 생성
+			final String token = tokenProvider.create(user);
 			final UserDTO responseUserDTO = UserDTO.builder()
 					.email(user.getEmail())
 					.id(user.getId())
+					.token(token)
 					.build();
 			
 			return ResponseEntity.ok().body(responseUserDTO);
